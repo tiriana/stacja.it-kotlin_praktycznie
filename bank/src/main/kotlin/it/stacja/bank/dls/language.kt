7feafab403ac8139b6a9d0ed035fd079e371875e
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-03-10 14:11:21 +0100

    rozszerzamy customera o adres

--- bank/src/main/kotlin/it/stacja/bank/dls/language.kt
+++ bank/src/main/kotlin/it/stacja/bank/dls/language.kt
@@ -1,9 +1,19 @@
 package it.stacja.bank.dls
 
+import it.stacja.bank.model.Address
 import it.stacja.bank.model.Customer
 
 fun customer(block: Customer.() -> Unit): Customer = Customer().apply(block);
 
+fun Customer.address(block: Address.() -> Unit) {
+    address = Address().apply(block);
+}
+
 val newCustomer = customer {
     name = "Kowalski"
+    address {
+        baseInfo = "Dąbrowskiego"
+        city = "Poznań"
+        postalCode = "61-234"
+    }
 }
