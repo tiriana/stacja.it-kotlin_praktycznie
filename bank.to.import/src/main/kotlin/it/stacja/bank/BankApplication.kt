70ff0fb20773a1feda0eaa770247150993b209c8
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-03-10 12:33:34 +0100

    hello controller

---
+++ bank.to.import/src/main/kotlin/it/stacja/bank/BankApplication.kt
@@ -0,0 +1,11 @@
+package it.stacja.bank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankApplication
+
+fun main(args: Array<String>) {
+    runApplication<BankApplication>(*args)
+}
