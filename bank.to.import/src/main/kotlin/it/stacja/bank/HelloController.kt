de7f140ced42b49f7c1a801e2cd2e9c19e095e9a
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-03-10 12:39:10 +0100

    hello endpoint

---
+++ bank.to.import/src/main/kotlin/it/stacja/bank/HelloController.kt
@@ -0,0 +1,17 @@
+package it.stacja.bank
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("sayHello")
+    fun sayHello(@RequestParam(
+            value = "name",
+            required = false,
+            defaultValue = "Kotlin"
+    ) name: String = "Is this important?") = "Hello $name"
+
+}
