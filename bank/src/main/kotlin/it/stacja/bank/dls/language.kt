c5f7b0211c456c10612cc12b85aed27e7852a8d9
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-03-10 14:05:30 +0100

    Skrótowo

--- bank/src/main/kotlin/it/stacja/bank/dls/language.kt
+++ bank/src/main/kotlin/it/stacja/bank/dls/language.kt
@@ -2,11 +2,7 @@ package it.stacja.bank.dls
 
 import it.stacja.bank.model.Customer
 
-fun customer(block: Customer.() -> Unit): Customer {
-    val customer = Customer()
-    block(customer)
-    return customer;
-}
+fun customer(block: Customer.() -> Unit): Customer = Customer().apply(block);
 
 val newCustomer = customer {
     name = "Kowalski"
