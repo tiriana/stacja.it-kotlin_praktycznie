a3f71e9f9225c5ba6465885bf8a8f84a040ab4f6
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-03-10 13:48:40 +0100

    inmemory SQL interafce. Now we can deveolop and add any SQL DB later

--- bank/src/main/kotlin/it/stacja/bank/model/Account.kt
+++ bank/src/main/kotlin/it/stacja/bank/model/Account.kt
@@ -5,7 +5,10 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @Entity
-data class Account(@GeneratedValue @Id val id: Long = 0, val number: String, val ballance: Long = 0) {
+data class Account(@GeneratedValue @Id val id: Long = 0, val number: String = "", val ballance: Long = 0) {
+
+    private constructor(): this(id = 0, number = "", ballance = 0) {}
+
     fun deposit(funds: Long): Account = copy(ballance = ballance + funds)
 
     fun withdraw(funds: Long): Account = copy(ballance = ballance - funds)
