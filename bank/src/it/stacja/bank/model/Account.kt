826dbca9c1020014920a231bf6f7e2e0da2c1c1d
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-03-10 10:40:48 +0100

    asd

--- bank/src/it/stacja/bank/model/Account.kt
+++ bank/src/it/stacja/bank/model/Account.kt
@@ -1,12 +1,13 @@
 package it.stacja.bank.model
 
 data class Account(val id: Long, val number: String, val ballance: Long) {
-    fun deposit(funds: Long) = copy(ballance = ballance + funds)
-    fun withdraw(funds: Long) {
+    fun deposit(funds: Long): Account = copy(ballance = ballance + funds)
+    fun withdraw(funds: Long): Account {
         if (ballance < funds) {
-            throw new InsufficentFundsException();
+            throw InsufficentFundsException();
         }
+        return copy(ballance = ballance - funds);
     }
 }
 
-class InsufficentFundsException: Throwable() {}
+class InsufficentFundsException: Throwable()
