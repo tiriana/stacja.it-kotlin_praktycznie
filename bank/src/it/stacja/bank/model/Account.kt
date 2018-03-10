d8b6c6a18a395336c3602606486b5df60c60f0c3
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-03-10 11:19:38 +0100

    account service

--- bank/src/it/stacja/bank/model/Account.kt
+++ bank/src/it/stacja/bank/model/Account.kt
@@ -1,6 +1,6 @@
 package it.stacja.bank.model
 
-data class Account(val id: Long = 0, val number: String, val ballance: Long) {
+data class Account(val id: Long = 0, val number: String, val ballance: Long = 0) {
     fun deposit(funds: Long): Account = copy(ballance = ballance + funds)
 
     fun withdraw(funds: Long): Account {
--- bank/src/it/stacja/bank/service/AccountService.kt
+++ bank/src/it/stacja/bank/service/AccountService.kt
@@ -1,2 +1,14 @@
 package it.stacja.bank.service
 
+import it.stacja.bank.repository.AccountRepository
+import it.stacja.bank.model.Account
+
+class AccountService(
+        val accountRepository: AccountRepository,
+        val accountNumberGenerator: AccountNumberGenerator) {
+
+    fun createAccount() {
+        val accountNumber: String = accountNumberGenerator.next
+        val account = Account(number = accountNumber);
+    }
+}
