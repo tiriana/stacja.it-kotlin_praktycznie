e7ca26e11e6328ede96bbafa9432583f9044e57a
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-03-10 10:44:01 +0100

    a

--- bank/src/it/stacja/bank/model/Account.kt
+++ bank/src/it/stacja/bank/model/Account.kt
@@ -2,6 +2,7 @@ package it.stacja.bank.model
 
 data class Account(val id: Long, val number: String, val ballance: Long) {
     fun deposit(funds: Long): Account = copy(ballance = ballance + funds)
+
     fun withdraw(funds: Long): Account {
         if (ballance < funds) {
             throw InsufficentFundsException();
--- bank/src/it/stacja/bank/model/reposotory.kt
+++ bank/src/it/stacja/bank/model/reposotory.kt
@@ -1,2 +1,12 @@
 package it.stacja.bank.model
 
+import it.stacja.bank.model.Account
+
+interface AccountRepository {
+    fun save(account: Account): Account
+
+    fun update(account: Account)
+
+    fun findByNumber(accountNumber: String): Account?
+}
+
